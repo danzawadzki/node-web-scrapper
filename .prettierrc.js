e66696dc74e7b460f2e8dcd1bb931776c5d4c881
Daniel Zawadzki <dan@ggpredict.io>
2022-08-15 20:43:39 +0200

    ci: Project initialization

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	bracketSpacing: true,
+	printWidth: 100,
+	jsxBracketSameLine: false,
+	quoteProps: 'consistent',
+	arrowParens: 'always',
+}
